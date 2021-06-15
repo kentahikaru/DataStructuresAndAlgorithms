05b0d162707f33ebedb8fca460c827f781e11a56
kentahikaru <quazarstar@gmail.com>
2021-06-16 00:05:29 +0200

    Added synopsis for the script

--- createProjects.ps1
+++ createProjects.ps1
@@ -1,3 +1,18 @@
+<#
+.SYNOPSIS
+createProjects.ps1 creates project structure of class library, tests and benchmark.
+.DESCRIPTION
+createProjects.ps1 creates a project structure consisting of project library, tests and benchmark.
+Class library name is name given as parameter. 
+Tests are named as input parameter + 'Tests'.
+Benchmark is named as input parameter + 'Benchmark'
+.PARAMETER where
+Where to create project structure.
+.PARAMETER what
+What to create. What is the (common) name for project(s).
+.EXAMPLE
+createProjects.ps1 . myApplication
+#>
 [CmdletBinding()]
 param (
 [Parameter(Mandatory=$True)]
