9d33c7e11f896d0b3bc893bf1779830a0f662500
kentahikaru <quazarstar@gmail.com>
2021-09-26 17:04:47 +0200

    [WIP] Knapsack Problem

---
+++ Algorithms/KnapsackProblem/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace KnapsackProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int capacity = 16;
+            int[] size = new int[] { 3,4,7,8,9};
+            int[] values = new int[] { 4,5, 10, 11,13};
+            int[] totval = new int[capacity];
+            int[] best = new int[capacity];
+            int n = values.Length;
+
+            for(int j = 0; j <= n-1; j++)
+            {
+                for(int i = 0; i <= capacity; i++)
+                {
+                    if(i >= size[j])
+                    {
+                        if(totval[i] < (totval[i-size[j]] + values[j]))
+                        {
+                            totval[i] = totval[i-size[j]] + values[j];
+                            best[i] = j;
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine("The maximum value is: " + totval[capacity]);
+        }
+    }
+}
